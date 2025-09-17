d55cd5a0e5e278bd35a2fc674e3f6947e5e2b8fb
Javier Arturo Raña Varela <turo@hotmail.es>
2025-09-17 12:16:33 +0200

    añadir botón "choose a photo" a pantalla de index

--- StickerApp/components/Button.tsx
+++ StickerApp/components/Button.tsx
@@ -1,10 +1,29 @@
 import { StyleSheet, View, Pressable, Text } from 'react-native';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 type Props = {
   label: string;
+  theme?: 'primary';
 };
 
-const Button = ({ label }: Props) => 
+const Button = ({ label,theme }: Props) => {
+    if (theme === 'primary') {
+  return (
+      <View
+        style={[
+          styles.buttonContainer,
+          { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
+        ]}>
+        <Pressable
+          style={[styles.button, { backgroundColor: '#fff' }]}
+          onPress={() => alert('You pressed a button.')}>
+          <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
+          <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
+        </Pressable>
+      </View>
+    );
+}
+  
   (
     <View style={styles.buttonContainer}>
       <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
@@ -12,6 +31,7 @@ const Button = ({ label }: Props) =>
       </Pressable>
     </View>
   );
+}
 
 const styles = StyleSheet.create({
   buttonContainer: {
@@ -29,6 +49,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row',
+  },
+  buttonIcon: {
+    paddingRight: 8,
   },
   buttonLabel: {
     color: '#fff',
